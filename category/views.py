d1a30e633286af9c4cccf2ce5c7d54e77863d54a
sea <Sea11rod2@gmail.com>
2023-02-27 19:04:55 +0530

    update view.py update home.html

--- category/views.py
+++ category/views.py
@@ -1,5 +1,11 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DeleteView, UpdateView
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DeleteView,
+    UpdateView,
+    DetailView,
+)
 from django.shortcuts import render
 from django.db.models import Sum
 from .models import category, category_info
@@ -45,30 +51,31 @@ class CatDeleteView(DeleteView):
     success_url = reverse_lazy("home")
 
 
-class CatViewInfo(ListView):
+class CatViewInfo(DetailView):
+    model = category
     template_name = "category_info.html"
-    context_object_name = "category_info_list"
+    # context_object_name = "category_list"
 
-    def get_queryset(self):
-        return category_info.objects.filter(cat=self.kwargs["pk"])
+    # def get_queryset(self):
+    #     return category_info.objects.filter(cat=self.kwargs["pk"])
 
-    def get_context_data(self, **kwargs):
-        dic = super().get_context_data(**kwargs)
+    # def get_context_data(self, **kwargs):
+    #     dic = super().get_context_data(**kwargs)
 
-        day = category_info.objects.values("date__day", "date__month").annotate(
-            category_sum=Sum("spend")
-        )
-        month = category_info.objects.values("date__month", "date__year").annotate(
-            category_sum=Sum("spend")
-        )
+    #     day = category_info.objects.values("date__day", "date__month").annotate(
+    #         category_sum=Sum("spend")
+    #     )
+    #     month = category_info.objects.values("date__month", "date__year").annotate(
+    #         category_sum=Sum("spend")
+    #     )
 
-        cat_wise = category_info.objects.values("cat__name").annotate(
-            category_sum=Sum("spend")
-        )
-        dic["cat_wise"] = cat_wise
-        dic["month"] = month
-        dic["day"] = day
-        return dic
+    #     cat_wise = category_info.objects.values("cat__name").annotate(
+    #         category_sum=Sum("spend")
+    #     )
+    #     dic["cat_wise"] = cat_wise
+    #     dic["month"] = month
+    #     dic["day"] = day
+    #     return dic
 
 
 class CatInfoAddView(CreateView):
