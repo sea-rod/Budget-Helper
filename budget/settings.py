1ffadffc9fa10400ab9408d924055e29bd424283
sea <Sea11rod2@gmail.com>
2023-03-29 22:07:32 +0530

    add mysql connection

--- budget/settings.py
+++ budget/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = "budget.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "budget",
+        "USER": "root",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
