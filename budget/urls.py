06fc7652e2ffcea656b74cf85beb17b8eb7971d6
sea <sea11rod@gmail.com>
2024-05-16 21:29:20 +0530

    removed deprecated package

--- budget/urls.py
+++ budget/urls.py
@@ -30,7 +30,6 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("v1/api/", include("api.urls")),
     path("api-auth/", include("rest_framework.urls")),
-    path("rest-auth/", include("rest_auth.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
