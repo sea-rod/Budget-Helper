43857e06c0c9ecff2d11d035a3ef65ddeb254bde
sea <sea11rod@gmail.com>
2024-08-02 17:52:36 +0530

    update settings n requirements

--- budget/settings.py
+++ budget/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".onrender.com"]
 
 
 # Application definition
